09b02ba17e869962f251c328b997546a06f0ab3b
David Král <david.kral67@gmail.com>
2023-05-21 17:37:50 +0200

    Initial commit

---
+++ src/main/java/cz/upce/fei/postolka_BE/PostolkaBeApplication.java
@@ -0,0 +1,13 @@
+package cz.upce.fei.postolka_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostolkaBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostolkaBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/upce/fei/postolka_BE/PostolkaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.upce.fei.postolka_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostolkaBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
