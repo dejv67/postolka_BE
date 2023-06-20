bbff8cf120a542f12b5d30d7d5e802674528b0d9
David Král <david.kral67@gmail.com>
2023-06-20 07:25:37 +0200

    one integration test without mocking

--- src/test/java/cz/upce/fei/postolka_BE/PostolkaBeApplicationTests.java
+++ src/test/java/cz/upce/fei/postolka_BE/PostolkaBeApplicationTests.java
@@ -1,17 +1,27 @@
 package cz.upce.fei.postolka_BE;
 
+import cz.upce.fei.postolka_BE.domain.Role;
+import cz.upce.fei.postolka_BE.domain.User;
+import cz.upce.fei.postolka_BE.repository.UserRepository;
+import cz.upce.fei.postolka_BE.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Transactional
 class PostolkaBeApplicationTests {
 
 	@Autowired
@@ -29,6 +39,37 @@ class PostolkaBeApplicationTests {
 				.andExpect(status().isOk())
 				.andExpect(jsonPath("$.name").value("prizemi 1"));
 	}
+
+
+	@Autowired
+	private UserService userService;
+
+	@Autowired
+	private UserRepository userRepository;
+	@Test
+	public void integrationTestCreateUser() throws Exception {
+
+		User user = new User();
+		user.setName("Lada");
+		user.setSurname("Tester");
+		user.setEmail("lada@tester.com");
+		user.setPassword("test");
+		user.setRole(Role.USER);
+		user.setModifDate(LocalDateTime.now());
+
+		userService.create(user);
+
+		User savedUser = userRepository.findById(user.getId()).orElse(null);
+
+		// Assert that the user was saved correctly
+		assertNotNull(savedUser);
+		assertEquals("Lada", savedUser.getName());
+		assertEquals("lada@tester.com", savedUser.getEmail());
+	}
+
+
+
+
 
 
 
